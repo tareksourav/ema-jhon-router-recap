2c6dc1a6031c7d9f895e9c6e905f8667ed3027ba
Ariful Islam <arifulislam161024@gmail.com>
2021-11-02 18:26:24 +0600

    inventory updated

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>Inventory</h2>
+            <h2>Inventory page updated</h2>
         </div>
     );
 };
